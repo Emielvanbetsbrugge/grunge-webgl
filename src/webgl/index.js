f8f81b70a2742056e0f9efbbfdcce6111a342d82
emielvanbetsbrugge <emiel.vanbetsbrugge@gmail.com>
2019-08-08 09:51:56 +0100

    added the curved rings

--- src/webgl/index.js
+++ src/webgl/index.js
@@ -2,11 +2,11 @@ import * as THREE from 'three';
 import { makeRings } from "./rings";
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 10000);
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setPixelRatio( window.devicePixelRatio );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+// renderer.setPixelRatio( window.devicePixelRatio );
+document.body.appendChild(renderer.domElement);
 
 camera.position.z = 200;
 
--- src/webgl/rings/index.js
+++ src/webgl/rings/index.js
@@ -1,8 +1,9 @@
 import * as THREE from 'three';
 
 const createGeometry = (innerWidth, depth, y) => {
+  console.log(y);
   const geometry = new THREE.RingGeometry(innerWidth, innerWidth + 4, 120);
-  const material = new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide});
+  const material = new THREE.MeshBasicMaterial({color: "#fff", side: THREE.DoubleSide});
   const mesh = new THREE.Mesh(geometry, material);
   mesh.position.y = y;
   mesh.position.z = depth;
@@ -11,8 +12,36 @@ const createGeometry = (innerWidth, depth, y) => {
 
 export const makeRings = () => {
   const meshes = [];
-  for(let i = 0; i < 20; i++) {
-    meshes[i] = createGeometry(70, 100 - 40 * i, 30);
+  const totalShapes = 100;
+  const totalTranslate = 0;
+  // let previousDeduction = 0;
+
+  for(let i = 0; i < totalShapes; i++) {
+    /* 
+     * So... what we're looking for right now is a way to fit the y between 2 values. 
+     * What could these values be? Right now they're between 0 & -760.
+     * I think I'll need a value to go between, 
+     * i becomes the value between numbers
+    */ 
+
+    // const percentageDone = i - totalShapes;
+    const percentage = (i * 100) / totalShapes;
+    // working some way
+    meshes[i] = createGeometry(70, 100 - 60 * i, totalTranslate - i * (1 * percentage));
+
+    // let's set a base and deduct a value off of that, let's increase that value, from the previous + an extra for each layer
+
+    // middle of the screen - previous deduction - (max deduction * percentage of items drawn)
+
+    // const percentage = (i * 100) / totalShapes;
+    // const base = 0;
+    // const maxDeduction = 20;
+
+    // const y = base - previousDeduction - (maxDeduction * percentage);
+    
+    // console.log(base, previousDeduction, maxDeduction, percentage);
+    // meshes[i] = createGeometry(70, 100 - 100 * i, y);
+    // previousDeduction = y;
   }
   return meshes;
 }
